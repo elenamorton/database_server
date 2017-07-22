f0606073b1c7b690f3391707008abc38cb205474
elenamorton <elena.uleia@gmail.com>
2017-07-22 19:03:58 +0100

    adding code for the get route with somekey

--- server.js
+++ server.js
@@ -24,6 +24,16 @@ server.get('/set', (req, res, next) => {
      .end('complete')
 })
 
+server.get('/get', (req, res) => {
+  let key = req.query.key
+
+  if (dataBase[key] !== undefined) {
+    res.send(`The result of: ${key} is ${dataBase[key]}`)
+  } else {
+    res.send(`Error: the record for ${key} is not found!`)
+  }
+})
+
 server.listen(4000, () => {
   console.log(`Server listening on Port: ${PORT}`)
 })
